88bc6088565c5fd626d576e6db889ea0d835f052
Renato <renatomna@gmail.com>
2023-10-05 17:05:02 -0300

    feat: :sparkles: Criação do GlobalStyles do projeto

---
+++ src/Styles/GlobalStyles.js
@@ -0,0 +1,30 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    width: 100%;
+    min-height: 100vh;
+    text-decoration: none;
+
+}
+
+a {
+    list-style: none;
+    cursor: pointer;
+    text-decoration: none;
+}
+
+button {
+    cursor: pointer;
+}
+
+li {
+    list-style: none;
+}
+`
