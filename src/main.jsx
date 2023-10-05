544aa93fd63360cf2f4dfd1d96c286cc4b885125
Renato <renatomna@gmail.com>
2023-10-05 17:27:37 -0300

    feat: :sparkles: feat: configuração inicial da landingPage e main.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './Styles/GlobalStyles.js'
+import { tema } from './Styles/theme'
+import LandingPage from './pages/LandingPage/LandingPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={tema}>
+      <GlobalStyle />
+      <LandingPage />
+    </ThemeProvider>
   </React.StrictMode>,
 )
