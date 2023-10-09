6348e5da39faf5a3cd23fa9a2f7d01a9e612d875
Renato <renatomna@gmail.com>
2023-10-09 15:31:20 -0300

    Feat: linpeza das pasta App e criação

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './Styles/GlobalStyles.js'
 import { tema } from './Styles/theme'
 import LandingPage from './pages/LandingPage/LandingPage.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={tema}>
       <GlobalStyle />
+      <App />
       <LandingPage />
     </ThemeProvider>
   </React.StrictMode>,
