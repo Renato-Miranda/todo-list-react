71466941769a71c8132017f25f24e160e75edd3b
Renato <renatomna@gmail.com>
2023-10-09 20:02:41 -0300

    feat: criação de useState para criação de tarefas.

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,27 @@ function App() {
       isCompleted: false,
     }
   ])
+
+  const addTodo = (text, category) => {
+
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+    }]
+    setTodos(newTodos)
+  }
+
   return (
     <div className='app'>
       <h1>To-Do List do Renatão</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todo todo={todo} />
+          <Todo key={todo.id} todo={todo} />
         ))}
       </div>
-      <TodoForm />
+      <TodoForm addTodo={addTodo} />
     </div>
 
   )
--- src/components/Todo/Form/TodoForm.jsx
+++ src/components/Todo/Form/TodoForm.jsx
@@ -1,11 +1,25 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
+    const [value, setValue] = useState('')
+    const [category, setCategory] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!value || !category) return
+        addTodo(value, category)
+        setValue('')
+        setCategory('')
+    }
+
     return (
         <div className="todo-form">
             <h2>Criar Tarefa:</h2>
-            <form >
-                <input type="text" placeholder="Digite Tarefa" />
-                <select>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Digite Tarefa" value={value} onChange={(e) => setValue(e.target.value)} />
+                <select value={category} onChange={(e) => setCategory(e.target.value)}>
                     <option value="">Selecione uma categoria</option>
                     <option value="Trabalho">Trabalho</option>
                     <option value="Pessoal">Pessoal</option>
