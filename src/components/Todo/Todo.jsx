509320f220958dd087b8c943a51499859b26d67f
Renato <renatomna@gmail.com>
2023-10-09 19:43:44 -0300

    feat: estilização dos botões de complete e remove

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -8,8 +8,8 @@ const Todo = ({ todo }) => {
                 <p className="category">({todo.category})</p>
             </div>
             <div>
-                <button>Completar</button>
-                <button>Del</button>
+                <button className="complete">Completar</button>
+                <button className="remove">Del</button>
             </div>
         </div>
     )
