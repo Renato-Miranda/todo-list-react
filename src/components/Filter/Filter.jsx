2a763205f3697e7c38fa46ccf636e85624737be9
Renato <renatomna@gmail.com>
2023-10-10 21:44:24 -0300

    feat: implementação da funcionalidade filtrar e de ordenar por ordem ascendente ou descendente as tarefas

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+const Filter = ({ filter, setFilter, setSort }) => {
+    return (
+        <div className="filter">
+            <h2>Filtrar:</h2>
+            <div className="filter-options">
+                <div>
+                    <p>Status:</p>
+                    <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                        <option value="All">Todas</option>
+                        <option value="Completed">Completas</option>
+                        <option value="Incomplete">Imcompletas</option>
+                    </select>
+                </div>
+                <div>
+                    <p>Ordem Alfabética:</p>
+                    <button onClick={() => setSort('Asc')}>Ascendente</button>
+                    <button onClick={() => setSort('Desc')}>Descendente</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Filter
