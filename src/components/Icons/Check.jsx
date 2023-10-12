45287ae0a280daf3c2902932e58b8463439dda8c
Renato <renatomna@gmail.com>
2023-10-12 15:04:41 -0300

    feat: criação dos componentes de icone check e lixeira

---
+++ src/components/Icons/Check.jsx
@@ -0,0 +1,11 @@
+import { BsCheck } from 'react-icons/bs'
+
+const Check = () => {
+    return (
+        <div>
+            <BsCheck size={15} color="#1a4159" />
+        </div>
+    )
+}
+
+export default Check
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { StylesTodo } from './todo.styles.js'
 import Lixeira from '../Icons/Lixeira.jsx'
+import Check from '../Icons/Check.jsx'
 const Todo = ({ todo, removeTodo, completeTodo }) => {
     return (
         <StylesTodo
@@ -10,7 +11,7 @@ const Todo = ({ todo, removeTodo, completeTodo }) => {
                 <p className="category">({todo.category})</p>
             </div>
             <div>
-                <button className="complete" onClick={() => completeTodo(todo.id)}>☑️</button>
+                <button className="complete" onClick={() => completeTodo(todo.id)}><Check /></button>
                 <button className="remove" onClick={() => removeTodo(todo.id)}><Lixeira /></button>
             </div>
         </StylesTodo>
